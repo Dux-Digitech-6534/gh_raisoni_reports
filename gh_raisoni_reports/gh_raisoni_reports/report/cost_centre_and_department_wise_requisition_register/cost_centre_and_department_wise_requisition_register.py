93997f667a44eb4a341904acdbf232e7ff00903e
Dux Digitech <dux@srv1215641.hstgr.cloud>
2026-04-27 11:09:45 +0000

    Changes On Repart 27/04/2026-4:39

--- gh_raisoni_reports/gh_raisoni_reports/report/cost_centre_and_department_wise_requisition_register/cost_centre_and_department_wise_requisition_register.py
+++ gh_raisoni_reports/gh_raisoni_reports/report/cost_centre_and_department_wise_requisition_register/cost_centre_and_department_wise_requisition_register.py
@@ -481,6 +481,152 @@
 #     return data
 
 
+# import frappe
+
+
+# def execute(filters=None):
+#     columns = get_columns()
+#     data = get_data(filters or {})
+#     return columns, data
+
+
+# def get_columns():
+#     return [
+#         {
+#             "label":"Material Indent ID",
+#             "fieldname":"name",
+#             "fieldtype":"Link",
+#             "options":"Material Indent",
+#             "width":180
+#         },
+#         {
+#             "label":"Transaction Date",
+#             "fieldname":"transaction_date",
+#             "fieldtype":"Date",
+#             "width":130
+#         },
+#         {
+#             "label":"Company",
+#             "fieldname":"company",
+#             "width":220
+#         },
+#         {
+#             "label":"Department",
+#             "fieldname":"department",
+#             "width":180
+#         },
+#         {
+#             "label":"Status",
+#             "fieldname":"custom_status",
+#             "width":180
+#         },
+#         {
+#             "label":"Item Code",
+#             "fieldname":"item_code",
+#             "width":180
+#         },
+#         {
+#             "label":"Quantity",
+#             "fieldname":"qty",
+#             "fieldtype":"Float",
+#             "width":100
+#         }
+#     ]
+
+
+# def get_data(filters):
+
+#     conditions = " WHERE mi.docstatus < 2 "
+
+#     if filters.get("from_date"):
+#         conditions += """
+#         AND mi.transaction_date >= %(from_date)s
+#         """
+
+#     if filters.get("to_date"):
+#         conditions += """
+#         AND mi.transaction_date <= %(to_date)s
+#         """
+
+#     if filters.get("item_code"):
+#         conditions += """
+#         AND mii.item_code=%(item_code)s
+#         """
+
+#     if filters.get("company"):
+#         conditions += """
+#         AND mi.company=%(company)s
+#         """
+
+#     if filters.get("custom_status"):
+#         conditions += """
+#         AND mi.custom_indent_status=%(custom_status)s
+#         """
+
+#     if filters.get("custom_department") and filters.get("custom_department")!="All Departments":
+#         conditions += """
+#         AND (
+#             EXISTS(
+#                 SELECT 1
+#                 FROM `tabMaterial Request` mr
+#                 WHERE mr.custom_material_indent=mi.name
+#                 AND mr.custom_department=%(custom_department)s
+#             )
+
+#             OR
+
+#             EXISTS(
+#                 SELECT 1
+#                 FROM `tabStock Entry` se
+#                 WHERE se.custom_material_indent=mi.name
+#                 AND se.custom_department=%(custom_department)s
+#             )
+#         )
+#         """
+
+
+#     data = frappe.db.sql(f"""
+#         SELECT
+#             mi.name,
+#             mi.transaction_date,
+#             mi.company,
+
+#             COALESCE(
+#                 (
+#                     SELECT se.custom_department
+#                     FROM `tabStock Entry` se
+#                     WHERE se.custom_material_indent=mi.name
+#                     LIMIT 1
+#                 ),
+#                 (
+#                     SELECT mr.custom_department
+#                     FROM `tabMaterial Request` mr
+#                     WHERE mr.custom_material_indent=mi.name
+#                     LIMIT 1
+#                 )
+#             ) as department,
+
+#             mi.custom_indent_status as custom_status,
+#             mii.item_code,
+#             mii.qty
+
+#         FROM
+#             `tabMaterial Indent` mi
+
+#         INNER JOIN
+#             `tabMaterial Request Item` mii
+#             ON mi.name = mii.parent
+
+#         {conditions}
+
+#         ORDER BY
+#             mi.transaction_date DESC
+
+#     """, filters, as_dict=1)
+
+#     return data
+
+
 import frappe
 
 
@@ -563,25 +709,16 @@ def get_data(filters):
         AND mi.custom_indent_status=%(custom_status)s
         """
 
-    if filters.get("custom_department") and filters.get("custom_department")!="All Departments":
+    # only department part changed
+    if filters.get("custom_department"):
         conditions += """
         AND (
-            EXISTS(
-                SELECT 1
-                FROM `tabMaterial Request` mr
-                WHERE mr.custom_material_indent=mi.name
-                AND mr.custom_department=%(custom_department)s
-            )
-
-            OR
-
-            EXISTS(
-                SELECT 1
-                FROM `tabStock Entry` se
-                WHERE se.custom_material_indent=mi.name
-                AND se.custom_department=%(custom_department)s
-            )
-        )
+            CASE
+                WHEN mi.custom_username LIKE '%% - %%'
+                THEN TRIM(SUBSTRING_INDEX(mi.custom_username,' - ',-1))
+                ELSE mi.custom_username
+            END
+        ) = %(custom_department)s
         """
 
 
@@ -591,20 +728,11 @@ def get_data(filters):
             mi.transaction_date,
             mi.company,
 
-            COALESCE(
-                (
-                    SELECT se.custom_department
-                    FROM `tabStock Entry` se
-                    WHERE se.custom_material_indent=mi.name
-                    LIMIT 1
-                ),
-                (
-                    SELECT mr.custom_department
-                    FROM `tabMaterial Request` mr
-                    WHERE mr.custom_material_indent=mi.name
-                    LIMIT 1
-                )
-            ) as department,
+            CASE
+                WHEN mi.custom_username LIKE '%% - %%'
+                THEN TRIM(SUBSTRING_INDEX(mi.custom_username,' - ',-1))
+                ELSE mi.custom_username
+            END as department,
 
             mi.custom_indent_status as custom_status,
             mii.item_code,
